7382fe84e56e89b71014d4d1c5a5ad26ad7c3be4
Julius kricheldorff <julius@kricheldorff.de>
2022-03-07 12:09:21 +0100

    final version of the epoching script to generate trials for the frequency transformation

--- Analysis/EpochFreq.m
+++ Analysis/EpochFreq.m
@@ -16,8 +16,8 @@ dbstop if error
 
 eegl                         = '/home/jules/Dropbox/PhD_Thesis/EEG_Labor/EEG_Software/eeglab2021.1';
 % set directories
-dirs.home                    = 'G:\Julia Ficke\AdaptiveControl\CleanData_EEG'; %hier habe ich das Gruppenlaufwerk gespeichert - du müsstest hier deinen Speicherort für die Daten eintragen
-dirs.eegsave                 = 'E:\AdaptiveControl\Data\FrequencyData'; % hier Ordner zum Speichern der Ergebenisse - im Gruppenlaufwerk unter PipelineValidate zu finden
+dirs.home                    = '/media/jules/DriveJules/AdaptiveControl/Data/FrequencyData/'; %hier habe ich das Gruppenlaufwerk gespeichert - du müsstest hier deinen Speicherort für die Daten eintragen
+dirs.eegsave                 = '/media/jules/DriveJules/AdaptiveControl/Data/FrequencyData/'; % hier Ordner zum Speichern der Ergebenisse - im Gruppenlaufwerk unter PipelineValidate zu finden
 
 %git directory name for me
 %  /C/Users/doex9445/Dateien/Julius/AdaptiveControl/tVNS-Project
@@ -35,7 +35,7 @@ Part_N                       = length(Participant_IDs); %number of participants
 % event onset codes
 onset =  {  'S 21'  'S 22'  'S 23'  'S 24'  'S 41'  'S 42'  'S 43' ...
     'S 44'  'S 61'  'S 62'  'S 63'  'S 64'  'S 81'  'S 82'  'S 83'  'S 84'  };
-epoch_dur = [-1.3  2];
+epoch_dur = [-2  2];
 
 
 % INSERT PARTICIPANTS WE WISH NOT TO ANALYZE
@@ -43,3 +43,21 @@ epoch_dur = [-1.3  2];
 %add EEGLAB path and start the program
 addpath(eegl);
 eeglab
+
+%% Epoching
+
+for sub = 1:Part_N
+    % get file location and load data
+    fileID                       = strcat(Participant_IDs{sub}, '_task-stroop_eeg_pruned_auto.set'); %get file ID
+    folderID                     = fullfile(dirs.home,Participant_IDs{sub});%get folder ID
+    EEG                          = pop_loadset('filename', fileID,'filepath',[folderID]); % load file
+    
+    % load data
+    EEG = pop_epoch( EEG,onset, epoch_dur, 'newname', 'Epochs for wavelet convolution', 'epochinfo', 'yes');
+    EEG = eeg_checkset( EEG );
+    
+    %save data
+    EEG = pop_saveset(EEG, 'filename',  strcat(Participant_IDs{sub}, '_epoched_freq'),'filepath',[folderID]); 
+    
+    % TECHNICAL DEBT CALCULATE HOW MANY TRIALS ARE LOST
+end
