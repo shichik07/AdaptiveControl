2023450fcc4f0a841c1f58f9fc4bbd097fa41b2a
Julius kricheldorff <julius@kricheldorff.de>
2022-04-13 15:45:04 +0200

    final

--- Analysis/get_trlindices.m
+++ Analysis/get_trlindices.m
@@ -9,6 +9,8 @@ Items.LWPC_MC_I =[];
 Items.LWPC_MI_C =[];
 Items.LWPC_MI_I =[];
 
+%Correct_check = {}; 
+
 
 
 for i=1:EEG.trials
@@ -16,6 +18,7 @@ for i=1:EEG.trials
     onset = cellfun(@(x)x == 0, EEG.epoch(i).eventlatency);
     
     if EEG.epoch(i).eventanswer{onset} == "correct" % proceed only with correctly answered trials
+        Correct_check{end+1} = EEG.epoch(i).eventanswer{onset};
         if EEG.epoch(i).eventanalysetype{onset} == "MI"
             if EEG.epoch(i).eventcongruency{onset} == "incongruent"
                 Items.LWPC_MI_I(end+1) = i;
@@ -43,5 +46,10 @@ for i=1:EEG.trials
         end
     end
 end
+
+%unique(cellfun(@unique, Correct_check)) % I believe at this poitn we only
+%have correct trials - but just in case
+
+
 end
   
