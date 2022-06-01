9b8585d43a160560834b50e5830a44e8f34d2355
shichik07 <julius.kricheldorff.de>
2022-06-01 17:08:39 +0200

    folder information for windows added

--- Analysis/TF_Ana_03_fieltrip_2_spm.m
+++ Analysis/TF_Ana_03_fieltrip_2_spm.m
@@ -9,23 +9,59 @@
 clear all; close all; clc
 dbstop if error
 
-eegl                         = '/home/jules/Dropbox/PhD_Thesis/EEG_Labor/EEG_Software/eeglab2021.1';
-ft                           = '/home/jules/Dropbox/PhD_Thesis/EEG_Labor/EEG_Software/fieldtrip-20220514';
-%ft2                          =
-%'/home/jules/Dropbox/PhD_Thesis/EEG_Labor/EEG_Software/fieldtrip'; %This
-%version doesn't work for transfering the data
-spm122                          = '/home/jules/Dropbox/PhD_Thesis/EEG_Labor/EEG_Software/spm12';
+% Windows folder locations
+spm122                       = 'C:\Program Files\MATLAB\EEGSoftware\spm12';
+eegl                         = 'C:\Program Files\MATLAB\EEGSoftware\eeglab2022.0';
 % set directories
-dirs.home                    = '/media/jules/DriveJules/AdaptiveControl/Data/FrequencyData/'; %hier habe ich das Gruppenlaufwerk gespeichert - du müsstest hier deinen Speicherort für die Daten eintragen
-dirs.eegsave                 = '/home/jules/Dropbox/PhD_Thesis/Adaptive_Control/Data/EEG/Raw/'; % hier Ordner zum Speichern der Ergebenisse - im Gruppenlaufwerk unter PipelineValidate zu finden
-dirs.functions               = '/home/jules/Dropbox/PhD_Thesis/Adaptive_Control/Analysis/Analysis/AdaptiveControl/Analysis';
+dirs.home                    = 'E:\AdaptiveControl\Data\FrequencyData\'; %hier habe ich das Gruppenlaufwerk gespeichert - du müsstest hier deinen Speicherort für die Daten eintragen
+dirs.eegsave                 = 'H:\KricheldorffJ\AdaptiveControl\Data\FrequencyData'; % hier Ordner zum Speichern der Ergebenisse - im Gruppenlaufwerk unter PipelineValidate zu finden
+dirs.functions               = 'C:\Users\doex9445\Dateien\Julius\AdaptiveControl\AdaptiveControl\Analysis';
 addpath(dirs.functions) 
 
+% Workspace on Windows PC
+% /C/Users/doex9445/Dateien/Julius/AdaptiveControl/AdaptiveControl 
+
+
+
+% % Linux folder locations
+% eegl                         = '/home/jules/Dropbox/PhD_Thesis/EEG_Labor/EEG_Software/eeglab2021.1';
+% ft                           = '/home/jules/Dropbox/PhD_Thesis/EEG_Labor/EEG_Software/fieldtrip-20220514';
+% %ft2                          =
+% %'/home/jules/Dropbox/PhD_Thesis/EEG_Labor/EEG_Software/fieldtrip'; %This
+% %version doesn't work for transfering the data
+% spm122                          = '/home/jules/Dropbox/PhD_Thesis/EEG_Labor/EEG_Software/spm12';
+% % set directories
+% dirs.home                    = '/media/jules/DriveJules/AdaptiveControl/Data/FrequencyData/'; %hier habe ich das Gruppenlaufwerk gespeichert - du müsstest hier deinen Speicherort für die Daten eintragen
+% dirs.eegsave                 = '/home/jules/Dropbox/PhD_Thesis/Adaptive_Control/Data/EEG/Raw/'; % hier Ordner zum Speichern der Ergebenisse - im Gruppenlaufwerk unter PipelineValidate zu finden
+% dirs.functions               = '/home/jules/Dropbox/PhD_Thesis/Adaptive_Control/Analysis/Analysis/AdaptiveControl/Analysis';
+
+addpath(dirs.functions) 
 addpath(eegl);
-addpath(ft);
+%addpath(ft);
 %addpath(ft2); % this version does not work anymore to transform the data to spm
 addpath(spm122)
 eeglab
+
+
+% Participant IDs we want to analyze            
+Participant_IDs              = dir(dirs.home);
+Participant_IDs              = Participant_IDs([Participant_IDs(:).isdir]); % remove all files (isdir property is 0)
+Participant_IDs              = Participant_IDs(~ismember({Participant_IDs(:).name},{'.','..'}));% remove '.' and '..'
+
+Participant_IDs          = Participant_IDs(~ismember({Participant_IDs(:).name},{'sub-PD_16'})); %remove PD 16, no data available and was excluded
+Participant_IDs          = Participant_IDs(~contains({Participant_IDs(:).name},{'derivatives'}));
+Participant_IDs          = Participant_IDs(~contains({Participant_IDs(:).name},{'sub-CY'})); %remove Data of young participants
+Participant_IDs              = {Participant_IDs(:).name};  
+Part_N                       = length(Participant_IDs); %number of participants
+
+% Make_new directory - needed to do it for a different hard drive - only apply it once
+% 
+% for sub = 1: Part_N
+%     dir_name = fullfile(dirs.eegsave,Participant_IDs{sub});
+%     mkdir(dir_name)
+% end
+
+
 
 
 EEGLABFILE = '/home/jules/Dropbox/PhD_Thesis/Adaptive_Control/Data/EEG/Raw/sub-CO_1_epoched_freq.set';
@@ -42,6 +78,17 @@ load(fileN)
 % accomplish this I use a code snippet by Karl Friston, Vladimir Litvak, 
 % which i adapted to my purposes 
 
+
+
+
+
+
+
+
+
+
+
+%% Actual Conversion
 % define the output file name
 %--------------------------------------------------------------------------
 fname = 'spm_conv.mat';
