5721a8f817c95591db9ca0c5334109489a0e8824
shichik07 <julius.kricheldorff.de>
2022-06-20 17:51:48 +0200

    finished conversion script

--- Analysis/TF_Ana_04_fieltrip_2_spm.m
+++ Analysis/TF_Ana_04_fieltrip_2_spm.m
@@ -12,14 +12,14 @@ dbstop if error
 % Windows folder locations
 spm122                       = 'C:\Program Files\MATLAB\EEGSoftware\spm12';
 eegl                         = 'C:\Program Files\MATLAB\EEGSoftware\eeglab2022.0';
+ft                           = 'C:\Program Files\MATLAB\Fieldtrip';
 % set directories
 dirs.home                    = 'E:\AdaptiveControl\Data\FrequencyData\'; %hier habe ich das Gruppenlaufwerk gespeichert - du müsstest hier deinen Speicherort für die Daten eintragen
 dirs.eegsave                 = 'H:\KricheldorffJ\AdaptiveControl\Data\FrequencyData'; % hier Ordner zum Speichern der Ergebenisse - im Gruppenlaufwerk unter PipelineValidate zu finden
 dirs.functions               = 'C:\Users\doex9445\Dateien\Julius\AdaptiveControl\AdaptiveControl\Analysis';
-addpath(dirs.functions) 
 
 % Workspace on Windows PC
-% /C/Users/doex9445/Dateien/Julius/AdaptiveControl/AdaptiveControl 
+% /C/Users/doex9445/Dateien/Julius/AdaptiveControl/AdaptiveControl
 
 
 
@@ -35,15 +35,14 @@ addpath(dirs.functions)
 % dirs.eegsave                 = '/home/jules/Dropbox/PhD_Thesis/Adaptive_Control/Data/EEG/Raw/'; % hier Ordner zum Speichern der Ergebenisse - im Gruppenlaufwerk unter PipelineValidate zu finden
 % dirs.functions               = '/home/jules/Dropbox/PhD_Thesis/Adaptive_Control/Analysis/Analysis/AdaptiveControl/Analysis';
 
-addpath(dirs.functions) 
+addpath(dirs.functions)
 addpath(eegl);
-%addpath(ft);
-%addpath(ft2); % this version does not work anymore to transform the data to spm
+addpath(ft);
 addpath(spm122)
 eeglab
 
 
-% Participant IDs we want to analyze            
+% Participant IDs we want to analyze
 Participant_IDs              = dir(dirs.home);
 Participant_IDs              = Participant_IDs([Participant_IDs(:).isdir]); % remove all files (isdir property is 0)
 Participant_IDs              = Participant_IDs(~ismember({Participant_IDs(:).name},{'.','..'}));% remove '.' and '..'
@@ -51,76 +50,70 @@ Participant_IDs              = Participant_IDs(~ismember({Participant_IDs(:).nam
 Participant_IDs          = Participant_IDs(~ismember({Participant_IDs(:).name},{'sub-PD_16'})); %remove PD 16, no data available and was excluded
 Participant_IDs          = Participant_IDs(~contains({Participant_IDs(:).name},{'derivatives'}));
 Participant_IDs          = Participant_IDs(~contains({Participant_IDs(:).name},{'sub-CY'})); %remove Data of young participants
-Participant_IDs              = {Participant_IDs(:).name};  
+Participant_IDs              = {Participant_IDs(:).name};
 Part_N                       = length(Participant_IDs); %number of participants
 
 % Make_new directory - needed to do it for a different hard drive - only apply it once
-% 
+%
 % for sub = 1: Part_N
 %     dir_name = fullfile(dirs.eegsave,Participant_IDs{sub});
 %     mkdir(dir_name)
 % end
 
 
-
-
-EEGLABFILE = '/home/jules/Dropbox/PhD_Thesis/Adaptive_Control/Data/EEG/Raw/sub-CO_1_epoched_freq.set';
-
-% hdr = ft_read_header( EEGLABFILE);
-% data = ft_read_data( EEGLABFILE, 'header', hdr );
-% events = ft_read_event( EEGLABFILE, 'header', hdr );
-% 
-fileN = fullfile(dirs.eegsave, 'testset.mat');
-save(fileN, 'hdr', 'data', 'events')
-load(fileN)
-
-% to convert fieldtrip data to spm I have to get my data in shape. To
-% accomplish this I use a code snippet by Karl Friston, Vladimir Litvak, 
-% which i adapted to my purposes 
-
-
-
-
-
-
-
-
-
-
-
-%% Actual Conversion
-% define the output file name
-%--------------------------------------------------------------------------
-fname = 'spm_conv.mat';
-save_loc = fullfile(dirs.eegsave, fname);
-
-% Create the Fieldtrip raw struct
-%--------------------------------------------------------------------------
-ftdata = [];
-
-for i = 1:size(data,3)
-   ftdata.trial{i} = data(:, :, i);
-   ftdata.time{i} = -2000:1000/hdr.Fs:2000-1000/hdr.Fs;
+for sub = 1:Part_N
+    % get file IDs
+    folderID                    = fullfile(dirs.home,Participant_IDs{sub});%get folder ID
+    folderID_t1                 = dir(fullfile(folderID, '**', '*_pl.mat'));
+    folderID_t2                 = dir(fullfile(folderID, '**', '*_npl.mat'));
+    folderID_t1                 = {folderID_t1(:).name}; %phase_locked files
+    folderID_t2                 = {folderID_t2(:).name}; %non_phase locked files
+    
+    
+    for p_type = 1:2 % do this for the phase locked and non-phase locked data
+        
+        if p_type == 1
+            files = folderID_t1;
+            file_type = 'TF_phase';
+        else
+            files = folderID_t2;
+            file_type = 'TF_non_phase';
+        end
+        
+        for con = 1:length(files)
+            load_loc = fullfile(dirs.eegsave, Participant_IDs{sub},  files{con});
+            data = load(load_loc);
+            if p_type == 1
+                data = data.TF_phase;
+            else
+                data = data.TF_non_phase;
+            end
+            
+            % convert data into fieldtrip like data structure
+            data = get_pseudo_FT_struct(data);
+            
+            % define save location - we save on the fast disk again
+            new_fn = files{con};
+            new_fn = strcat(new_fn(1:end-4), '_spm.mat');
+            save_loc = fullfile(dirs.home, Participant_IDs{sub}, new_fn);
+            
+            % convert to spm format
+            D = spm_eeg_ft2spm(data, new_fn);
+            
+            % Examples of providing additional information in a script
+            % [] comes instead of an index vector and means that the command
+            % applies to all channels/all trials.
+            %--------------------------------------------------------------------------
+            D = type(D, 'single');             % Sets the dataset type
+            D = chantype(D, [], 'EEG');        % Sets the channel type
+            subn_l = length(Participant_IDs{sub}); % get length of subject name
+            D = conditions(D, [], new_fn(subn_l+2:subn_l+10);  % Sets the condition label
+            
+            % save
+            %--------------------------------------------------------------------------
+            
+            save(D);
+            clear data D
+        end
+    end
 end
-
-ftdata.fsample = hdr.Fs; %sampling rate
-ftdata.label = hdr.label(:); %electrode labels
-ftdata.hdr = hdr;
-
-% Convert the ftdata struct to SPM M\EEG dataset
-%--------------------------------------------------------------------------
-D = spm_eeg_ft2spm(ftdata, save_loc);
-spm_eeg_ft2spm(ftdata)
-% Examples of providing additional information in a script
-% [] comes instead of an index vector and means that the command
-% applies to all channels/all trials.
-%--------------------------------------------------------------------------
-D = type(D, 'single');             % Sets the dataset type
-D = chantype(D, [], 'EEG');        % Sets the channel type 
-D = conditions(D, [], 'Sound 1');  % Sets the condition label
-
-% save
-%--------------------------------------------------------------------------
-
-save(D);
-
