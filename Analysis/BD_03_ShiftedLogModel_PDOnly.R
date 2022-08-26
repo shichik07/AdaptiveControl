0fabcd3c298ab1f56f94b042afea39366a3a9425
shichik07 <julius.kricheldorff.de>
2022-08-26 13:03:48 +0200

    First random subject specific effects model with correlated slopes

--- Analysis/BD_03_ShiftedLogModel_PDOnly.R
+++ Analysis/BD_03_ShiftedLogModel_PDOnly.R
@@ -56,6 +56,58 @@ df <- df %>%
   select(response_time_Keyboard_response, Age, Congruency, Gender, Analysis_type, Exp_group, Subject, Trl_type, correct_Keyboard_response, Block, Item, Item_specific, Congruency_main, UPDRS) %>%
   dplyr::rename(RT = response_time_Keyboard_response, Correct_A = correct_Keyboard_response)
 
+# Contrasts only for the list-wide effect only
+StroopCon_LW <- hypr(
+  PD_Congruency = (PD_LW_incon_C + PD_LW_con_C)/2 ~ (PD_LW_con_I + PD_LW_incon_I)/2, # main effect Congruence healthy controls
+  PD_Listwide = (PD_LW_con_C + PD_LW_con_I)/2 ~ (PD_LW_incon_C + PD_LW_incon_I)/2, # main effect Block Listwide control
+  PD_LW_Block = (PD_LW_incon_I - PD_LW_incon_C)/2 ~ (PD_LW_con_I - PD_LW_con_C)/2, # interaction listwide effect
+  levels = c("PD_LW_incon_I", "PD_LW_incon_C", "PD_LW_con_C", "PD_LW_con_I")
+)
+StroopCon_LW
+
+# Contrasts for the Item-specific effects only - check that later (might be more difficult)
+StroopCon_IS <- hypr(
+  PD_Congruency = (PD_IS_incon_C + PD_IS_con_C)/2 ~ (PD_IS_con_I + PD_IS_incon_I)/2, # main effect Congruence healthy controls
+  PD_Itemspecific = (PD_IS_con_C + PD_IS_con_I)/2 ~ (PD_IS_incon_C + PD_IS_incon_I)/2, # main effect Block Listwide control
+  PD_IS_Block = (PD_IS_incon_I - PD_IS_incon_C)/2 ~ (PD_IS_con_I - PD_IS_con_C)/2, # interaction listwide effect
+  levels = c("PD_IS_con_C", "PD_IS_con_I", "PD_IS_incon_I", "PD_IS_incon_C")
+)
+StroopCon_IS
+
+
+# Now we create an extra column for the contrasts for the List wide effect
+df <- df %>%
+  mutate(Contrast_LW = case_when(
+    Analysis_type == "MC" & Congruency == "congruent"  & Exp_group == "PD" ~ "PD_LW_con_C",
+    Analysis_type == "MC" & Congruency == "incongruent" & Exp_group == "PD"  ~ "PD_LW_con_I",
+    Analysis_type == "MI" & Congruency == "congruent"  & Exp_group == "PD" ~ "PD_LW_incon_C",
+    Analysis_type == "MI" & Congruency == "incongruent"  & Exp_group == "PD" ~ "PD_LW_incon_I",
+  )) %>%
+  mutate(Contrast_LW  = as_factor(Contrast_LW)) 
+
+# assign the generated contrast matrix to the List Wide Factor
+contrasts(df$Contrast_LW) <- contr.hypothesis(StroopCon_LW)
+
+contrasts(df$Contrast_LW)    
+
+# Now we create an extra column for the contrasts for the Item Specific effect
+df <- df %>%
+  mutate(Contrast_IS = case_when(
+    Analysis_type == "main_con" & Congruency == "congruent"  & Exp_group == "PD" ~ "PD_IS_con_C",
+    Analysis_type == "main_con" & Congruency == "incongruent"  & Exp_group == "PD" ~ "PD_IS_con_I",
+    Analysis_type == "main_incon" & Congruency == "congruent"  & Exp_group == "PD" ~ "PD_IS_incon_C",
+    Analysis_type == "main_incon" & Congruency == "incongruent"  & Exp_group == "PD" ~ "PD_IS_incon_I",
+  )) %>%
+  mutate(Contrast_IS  = as_factor(Contrast_IS))
+
+# assign the generated contrast matrix to the Item Specific Factor
+contrasts(df$Contrast_IS) <- contr.hypothesis(StroopCon_IS)
+
+contrasts(df$Contrast_IS)    
+
+
+
+
 ## Get seperate datasets
 Data_y_inducer_LWPC <- df %>%
   dplyr::filter(Item_specific == "List_wide") %>% # only Listwide items
@@ -90,25 +142,41 @@ Data_y_diagnostic_ISPC <- df %>%
   mutate(Congruency  = as_factor(Congruency)) %>%
   mutate(Analysis_type = as_factor(Analysis_type))
 
+# brmsformula object List Wide
+m2_PD_LW <- bf(RT ~ 1 + Contrast_LW + (Contrast_LW|Subject) + (1|Item)) 
+a <- get_prior(data = Data_y_inducer_LWPC, m2_PD_LW, family = shifted_lognormal()) 
+
 ## Weakly informative prior for the listwide and item specific effect
 
 # Prior informed weakly List wide
 prior_weakly_informed_LW <- c(
   prior(student_t(3, 6.5, 0.5), class = Intercept), # group effect non-decision time CO is 0 in the contrast we assume on average 200ms
   prior(cauchy(0,0.5), class = sigma), 
-  prior(cauchy(0, 5), class = ndt), 
-  prior(normal(0,0.5), class = b, coef = Analysis_typeMC), # Priors of Contrasts for CO
-  prior(normal(0,0.5), class = b, coef = Congruencycongruent),
-  prior(normal(0,0.5), class = b, coef = Congruencycongruent:Analysis_typeMC),
+  prior(cauchy(0, 5), class = ndt),
+  prior(lkj(1), class = cor), # use default prior for the correlation 
+  prior(lkj(1), class = cor, group = Subject),
   prior(normal(0,0.5), class = b, coef = Contrast_LWPD_Congruency), # Priors of Contrasts for PD patients
   prior(normal(0,0.5), class = b, coef = Contrast_LWPD_Listwide),
   prior(normal(0,0.5), class = b, coef = Contrast_LWPD_LW_Block),
   prior(normal(0, 0.5), class = sd, coef = Intercept, group = Subject),
+  prior(normal(0, 0.5), class = sd, coef = Contrast_LWPD_Congruency, group = Subject),
+  prior(normal(0, 0.5), class = sd, coef = Contrast_LWPD_Listwide, group = Subject),
+  prior(normal(0, 0.5), class = sd, coef = Contrast_LWPD_LW_Block, group = Subject),
   prior(normal(0, 0.5), class = sd, coef = Intercept, group = Item)
 )
 
+fit_ShiftedLog_PDOnly_inducer_LW <- brm(formula = m2_PD_LW ,
+                            family = shifted_lognormal(),
+                            data = Data_y_inducer_LWPC,
+                            prior = prior_weakly_informed_LW,
+                            warmup = 2000,
+                            iter = 12000,# 20000 is the limit necessary for bridge sampling
+                            cores = 4, seed = 423,
+                            control = list(adapt_delta = 0.95),
+                            #save_pars = save_pars(all = TRUE), # must be set to true for bridgesampling
+                            chains =4
+)
 
-# brmsformula object List Wide
-m2_PD_LW <- bf(RT ~ 1 + Congruency + Analysis_type + Congruency*Analysis_type  + (Subject|Congruency*Analysis_type) + (1|Subject) + (1|Item)) 
+save(fit_ShiftedLog_PDOnly_inducer_LW, file = "fit_ShiftedLog_PDOnly_inducer_LW.rda")
 
-a <- get_prior(data = Data_y_inducer_LWPC, m2_PD_LW, family = shifted_lognormal())         
+
